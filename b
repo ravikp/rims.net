91cce4413c67f09d102f45638eecebb7cef9481d
RAVI KUMAR PASUMARTHY <ravi.pasumarthy@gmail.com>
2011-07-23 02:20:16 +0530

    first commit to have all rims excel formulae's in one place

---
+++ b
@@ -0,0 +1,3 @@
+#! /bin/env bash
+
+tools/nant/bin/Nant.exe $*
---
+++ rims.formulae/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExcelDna.Integration;
+
+namespace rims.formulae
+{
+    public class StringExtensions
+    {
+        [ExcelFunction(
+            Description = "Given the age in years, the function then calculates the Birth year.", 
+            Category = "RIMS.NET")]
+        public static int YearOfBirth(int ageInYears)
+        {
+            return DateTime.Now.Year - ageInYears;
+        }
+    }
+}
